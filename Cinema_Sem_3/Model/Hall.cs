67f4b5967090cd594c26d7873c5b9421b9a49760
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-03-13 12:56:39 +0100

    Added Scheduler to Model Layer

---
+++ Cinema_Sem_3/Model/Hall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class Hall
+    {
+        private int hallID;
+        private string maxNumberOfSeats;
+        private string hallName;
+
+        public string getSetHallName
+        {
+            get { return hallName; }
+            set { hallName = value; }
+        }
+
+        public int getSetHallID
+        {
+            get { return hallID; }
+            set { hallID = value; }
+        }
+
+        public string getSetMaxNumberOfSeats
+        {
+            get { return maxNumberOfSeats; }
+            set { maxNumberOfSeats = value; }
+        }
+    }
+}
---
+++ Cinema_Sem_3/Model/Scheduler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class Scheduler
+    {
+        private int schID;
+        private DateTime date;
+        private int timeInMinutes;
+        private Movie movie;
+        private Hall hall;
+        private int endTimeInMinutes;
+
+        public int SchID
+        {
+            get { return schID; }
+            set { schID = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public int TimeInMinutes
+        {
+            get { return timeInMinutes; }
+            set { timeInMinutes = value; }
+        }
+
+        internal Movie Movie
+        {
+            get { return movie; }
+            set { movie = value; }
+        }
+
+        internal Hall Hall
+        {
+            get { return hall; }
+            set { hall = value; }
+        }
+
+        public int EndTimeInMinutes
+        {
+            get { return endTimeInMinutes; }
+            set { endTimeInMinutes = value; }
+        }
+    }
+}
