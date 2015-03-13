e4e98169ce23a2bd6d40b9bbbfd7b025bb0f1e0e
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-03-13 12:36:31 +0100

    Added Movie to Model Layer

---
+++ Cinema_Sem_3/Model/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class Movie
+    {
+        private int id;
+        private string name;
+        private int playTime;
+
+        public int getSetID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int getSetPlayTime
+        {
+            get { return playTime; }
+            set { playTime = value; }
+        }
+
+        public string getSetName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
