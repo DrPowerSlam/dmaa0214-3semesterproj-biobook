069fb0733f9e6305fa0a0ed2f3a0286a7a1b753d
1030874 <1030874@ucn.dk>
2015-03-13 12:31:41 +0100

    hehe

---
+++ Cinema_Sem_3/Model/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Customer
+    {
+
+        private string Name;
+        public string GetName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        private int ID;
+        public int GetID
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+
+        public Customer(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+
+
+    }
+}
