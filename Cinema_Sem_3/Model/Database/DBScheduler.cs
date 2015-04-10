33fe1c835534de43e7cf53c08cdc804de884819b
commentatorboy <commentatorbot@gmail.com>
2015-04-10 14:37:14 +0200

    Testing connection to database

--- Cinema_Sem_3/Model/Database/DBScheduler.cs
+++ Cinema_Sem_3/Model/Database/DBScheduler.cs
@@ -5,9 +5,35 @@ using System.Text;
 
 namespace Model.Database
 {
-    class DBScheduler
+    public class DBScheduler
     {
         //Use LINQ to get the schedulers where the schedulerID is x. (See inside SchedulerController.cs)
-        
+        public Scheduler GetScheduler(int schedulerID)
+        {
+
+            
+            Scheduler test = new Scheduler();
+            
+            var db = new ConnectToDBDataContext();
+
+            var query2 = db.Schedulers.Select(x => x).AsEnumerable().Where(x => x.SchID == schedulerID);
+
+            var scheduler =     (from schDB in db.Schedulers
+                                where schDB.SchID == schedulerID 
+                                select new Scheduler
+                                    {
+                                        SchID = schDB.SchID,
+                                        Datetime = schDB.Datetime
+                                    }).ToList<Scheduler>();
+
+
+            
+
+
+            return scheduler.First<Scheduler>();
+
+
+
+        }
     }
 }
