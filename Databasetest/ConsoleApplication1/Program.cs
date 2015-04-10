0a86630415473a47fec29b12c9370720578b4ec5
René Dahl <1030874@ucn.dk>
2015-04-10 12:31:46 +0200

    Update app

--- Databasetest/ConsoleApplication1/Program.cs
+++ Databasetest/ConsoleApplication1/Program.cs
@@ -31,6 +31,41 @@ namespace ConsoleApplication1
             }
  Console.ReadLine();
 
+
+
+
+ // using (SqlConnection cn = new SqlConnection())
+ //            {
+ //                cn.ConnectionString = "Data Source=kraka.ucn.dk;Initial Catalog=dmaa0214_3Sem_2;Persist Security Info=True;User ID=dmaa0214_3sem_2;Password=IsAllowed";
+ //                cn.Open();
+ //             string strSQL = "Select * from Hall"; // define sql statement
+ //                using (SqlCommand myCommand = new SqlCommand(strSQL, cn))
+ //                {
+ //                   using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+ //                   {
+ //                       while (myDataReader.Read())
+ //                       {
+ //                           Console.WriteLine("HallID:{0}, Name:{1}",
+ //                           myDataReader["MaxNumberOfSeats"].ToString(),
+ //                           myDataReader["MaxNumberOfSeats"].ToString());
+ //                       }
+ //                   }
+ //               }
+ //           }
+ //Console.ReadLine();
+
+ //       }
+
+
+
+
+
+
+
+
+
+
+
         }
         }
 }
