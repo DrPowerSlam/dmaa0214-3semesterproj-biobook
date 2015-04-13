f2368ebd42e1adf998d7ab724d1b421edf15d963
1030874 <1030874@ucn.dk>
2015-04-13 12:12:49 +0200

    RD
    Prøver at update

---
+++ Cinema_Sem_3/Model/CinemaService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Model
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CinemaService" in both code and config file together.
+    public class CinemaService : ICinemaService
+    {
+        public void GetScheduler(int schedulerID)
+        {
+            //Get the scheduler from the SchedulerController.cs
+            //Show that scheduler
+            GetScheduler(schedulerID);
+        }
+    }
+}
---
+++ Cinema_Sem_3/Model/Database/DBScheduler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Database
+{
+    public class DBScheduler
+    {
+        //Use LINQ to get the schedulers where the schedulerID is x. (See inside SchedulerController.cs)
+        public Scheduler GetScheduler(int schedulerID)
+        {
+            
+            var db = new ConnectToDBDataContext();
+
+            var scheduler = db.Schedulers.Select(x => x).AsEnumerable().Where(x => x.SchID == schedulerID);
+
+            return scheduler.First<Scheduler>();
+        }
+    }
+}
