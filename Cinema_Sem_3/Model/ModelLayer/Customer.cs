db9f1624abeea0de8d0547432cd1b8dc6297774d
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-04-13 13:02:04 +0200

    Test

--- Cinema_Sem_3/Model/ModelLayer/Customer.cs
+++ Cinema_Sem_3/Model/ModelLayer/Customer.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq.Mapping;
 
 namespace ServerProject.ModelLayer
 {
-    public class Customer
+    [Table(Name="Customer")]
+    public partial class Customer
     {
 
         private string name;
