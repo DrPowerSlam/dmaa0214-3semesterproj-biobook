a8355a668fbd4c4eaa50a82484d26b9320de789a
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-04-22 11:23:43 +0200

    Comments for customer classes

--- ServerProject/ServerProject/ControllerLayer/CustomerController.cs
+++ ServerProject/ServerProject/ControllerLayer/CustomerController.cs
@@ -13,37 +13,48 @@ namespace ServerProject.ControllerLayer
 
         DBCustomer dbCus = new DBCustomer();
 
+        /// <summary>
+        /// Gets all the customers from the databaselayer
+        /// </summary>
+        /// <returns>Returns the list of customers</returns>
         public IEnumerable getAllCustomers()
         {
             return dbCus.getAllCustomers();
         }
 
+        /// <summary>
+        /// Finds a customer by it's ID
+        /// </summary>
+        /// <param name="ID">The ID to search for</param>
+        /// <returns>The customer, if any is found</returns>
         public Customer findCustomerByID(int ID)
         {
             return dbCus.selectWhereID(ID);
         }
 
-        public ArrayList findCustomersByName(string name)
+        /// <summary>
+        /// Finds all the customers with a given name
+        /// </summary>
+        /// <param name="name">The name to search for</param>
+        /// <returns>Returns a list of customers</returns>
+        public IEnumerable findCustomersByName(string name)
         {
-            ArrayList customerList = new ArrayList();
-            foreach (Customer c in dbCus.selectWhereName(name))
-            {
-                if (c.name == name)
-                {
-                    Customer customerModel = new Customer();
-                    customerModel.CusID = c.CusID;
-                    customerModel.name = c.name;
-                    customerList.Add(customerModel);
-                }
-            }
-            return customerList;
+            return dbCus.selectWhereName(name);
         }
 
+        /// <summary>
+        /// Inserts a customer to the database
+        /// </summary>
+        /// <param name="name">The name of the customer (ID is auto-generated)</param>
         public void insertCustomer(string name)
         {
             dbCus.insertCustomer(name);
         }
 
+        /// <summary>
+        /// Deletes a customer with a given ID
+        /// </summary>
+        /// <param name="cusID">The ID of the customer to delete</param>
         public void deleteCustomer(int cusID)
         {
             dbCus.deleteCustomer(dbCus.selectWhereID(cusID));
