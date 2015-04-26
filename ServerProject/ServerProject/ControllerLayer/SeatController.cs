6e838844669a9ac2197b73a2de7db7ed5fb18b11
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-04-26 23:34:52 +0200

    Moved methods to controller
    From DBSeat to SeatController

--- ServerProject/ServerProject/ControllerLayer/SeatController.cs
+++ ServerProject/ServerProject/ControllerLayer/SeatController.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ServerProject.DatabaseLayer;
 
 namespace ServerProject.ControllerLayer
 {
     class SeatController
     {
+        public Array ConvertStringToArray(int schID)
+        {
+            DBSeat dbSeat = new DBSeat();
+            var seatListoperator = dbSeat.GetSeats(schID);
+            int count = seatListoperator.Count();
+
+
+            int[][] jagged = new int[count][];
+
+            for (int row = 0; row < count; row++)
+            {
+                Seat s = seatListoperator.ElementAt(row);
+                //int tempCount = (int)s.ColumnArray.Count();
+                int i = 0;
+                int lengthChar = (1 + s.ColumnArray.Count()) / 2;
+                jagged[row] = new int[lengthChar];
+                Console.WriteLine("length: " + lengthChar);
+                foreach (char c in s.ColumnArray)
+                {
+                    if (!c.Equals(','))
+                    {
+                        jagged[row][i] = (int)Char.GetNumericValue(c);
+                        i++;
+                    }
+                }
+            }
+
+            return jagged;
+        }
     }
 }
