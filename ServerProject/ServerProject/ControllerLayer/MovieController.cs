a9a3648ffd4be0c36f006ec0a25bf63eb84699cc
DrPowerSlam <Tue_Brodersen@hotmail.com>
2015-04-27 14:50:18 +0200

    Added comments

--- ServerProject/ServerProject/ControllerLayer/MovieController.cs
+++ ServerProject/ServerProject/ControllerLayer/MovieController.cs
@@ -11,26 +11,49 @@ namespace ServerProject.ControllerLayer
     {
         DBMovie dbMovie = new DBMovie();
 
+        /// <summary>
+        /// Gets all the movies from the database
+        /// </summary>
+        /// <returns>Returns an IEnumerable with all the movies</returns>
         public IEnumerable getAllMovies()
         {
             return dbMovie.getAllMovies();
         }
 
+        /// <summary>
+        /// Finds a movie with a given ID
+        /// </summary>
+        /// <param name="ID">The ID of the movie you want to find</param>
+        /// <returns>Returns the movie as a movie object</returns>
         public Movie findMovieByID(int ID)
         {
             return dbMovie.selectWhereID(ID);
         }
 
+        /// <summary>
+        /// Find a movie by a given name
+        /// </summary>
+        /// <param name="name">The name of the movie you want to find</param>
+        /// <returns>Returns the movie as a movie object</returns>
         public Movie findMovieByName(string name)
         {
             return dbMovie.selectWhereName(name);
         }
 
+        /// <summary>
+        /// Inserts a movie into the database
+        /// </summary>
+        /// <param name="name">The name of the movie</param>
+        /// <param name="playTime">The playtime of the movie</param>
         public void insertMovie(string name, int playTime)
         {
             dbMovie.insertMovie(name, playTime);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="movie"></param>
         public void deleteMovie(Movie movie)
         {
             dbMovie.deleteCustomer(movie);
