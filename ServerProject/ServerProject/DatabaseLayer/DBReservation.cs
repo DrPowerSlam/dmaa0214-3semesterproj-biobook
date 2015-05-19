348f89f0aed5076e033eaaa79e1ca0b11abd3aa2
Hamun <sijdfvno@gmail.com>
2015-05-19 10:27:03 +0200

    Added transaction again

--- ServerProject/ServerProject/DatabaseLayer/DBReservation.cs
+++ ServerProject/ServerProject/DatabaseLayer/DBReservation.cs
@@ -8,16 +8,18 @@ using System.Collections;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Threading;
+//using System.Transactions;
 
 namespace ServerProject.DatabaseLayer
 {
     class DBReservation
     {
-        private object lockToken = new object();
-        ConnectToDatabaseDataContext db = new ConnectToDatabaseDataContext();
+        private object lockToken = new object();        
+
         public IEnumerable getReservation()
         {
-            
+            ConnectToDatabaseDataContext db = new ConnectToDatabaseDataContext();
             DBCustomer dbCustomer = new DBCustomer();
             List<Customer> customers = dbCustomer.GetAllCustomers().Cast<Customer>().ToList();
             List<Reservation> reservation = db.Reservations.Select(x => x).AsEnumerable().ToList();
@@ -29,22 +31,65 @@ namespace ServerProject.DatabaseLayer
             return cusResJoin;
         }
 
+        public void UpdateReservation(Reservation res, int sleepTime)
+        {
+            ConnectToDatabaseDataContext db = new ConnectToDatabaseDataContext();
+            db.Connection.Open();
+            db.Transaction = db.Connection.BeginTransaction(IsolationLevel.RepeatableRead);
+            Reservation reservation = db.Reservations.First(r => r.ResID == 4);
+            
+            //Console.WriteLine(reservation.Row);
+            //using (var transation = new TransactionScope() )
+            //{
+
+                reservation.CustomerID = 1;
+                reservation.Row = "1,2,3,4";
+                reservation.Seat = "5,5,5,5,1,4,";
+                reservation.SchedulerID = 1;
+                //reservation.ResID = 1;
+
+                Console.WriteLine("Thread is now sleeping");
+                
+                try
+                {
+
+                    Console.WriteLine("Thread is now not sleeping");
+                    db.SubmitChanges();
+                    Thread.Sleep(10000);
+                    db.Transaction.Commit();
+                    db.Transaction.Dispose();
+                    //transation.Complete();
+                    db.Connection.Close();
+
+                }
+                catch (Exception e)
+                {
+                    db.Transaction.Rollback();
+                    Console.WriteLine(e);
+                }
+            //}
+        }
+
+
         public void insertReservation(Reservation res)
         {
             //make it a reservation
-           
             Reservation reservation = new Reservation();
             reservation = res;
 
             //lock it so only one at a time can work with it?
-
+            ConnectToDatabaseDataContext db = new ConnectToDatabaseDataContext();
             db.Reservations.InsertOnSubmit(reservation);
             try
             {
+                Thread.Sleep(2000);
+                Console.WriteLine("Thread is now not sleeping");
                 db.SubmitChanges();
+                
             }
             catch (Exception e)
             {
+                db.Transaction.Rollback();
                 Console.WriteLine(e);
             }
 
