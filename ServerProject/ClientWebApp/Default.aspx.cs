0987e66928b54523e89304e2a4db88b901a0a823
1030874 <1030874@ucn.dk>
2015-05-22 14:45:54 +0200

    addd adduser, samt defaukt.cs

--- ServerProject/ClientWebApp/Default.aspx.cs
+++ ServerProject/ClientWebApp/Default.aspx.cs
@@ -22,12 +22,11 @@ namespace ClientWebApp
 
                 List<Movie> movieList = client.GetAllMovies();
                 
-                foreach(Movie movie in movieList)
-                {
-                    litNewstMovies.Text += movie.name;
-                }
+                //foreach(Movie movie in movieList)
+                //{
+                //    litNewstMovies.Text += movie.name + "<br/>";
+                //}
 
-                
 
                 litNewstMovies.Text += "<div class=\"row\">";
 
@@ -44,36 +43,36 @@ namespace ClientWebApp
 
                 litNewstMovies.Text += "<div class=\"row text-center\">";
 
-                //foreach (Scheduler scheduler in client.GetAllSchedulers())
-                //{
-              
-
-                //    litNewstMovies.Text += "<div class=\"col-md-3 col-sm-6\">";
-                //    litNewstMovies.Text += "<div class=\"thumbnail\">";
-
-                //    litNewstMovies.Text += "<a href=\"Resever/"   + movie.MovieID + movie.name + "\"><img src='../../Content/Images/" + movie.Image + " ' alt='' title='' border=\"0\" /></a>";
-                //    litNewstMovies.Text += "<a href=\"Resever/" + scheduler.Movie.name + "\"><img src='../../Content/Images/" + scheduler.Movie.Image + " ' alt='' title='' border=\"0\" /></a>";
-                //    litNewstMovies.Text += "<div class=\"caption\">";
-                //    litNewstMovies.Text += "<h3>" + scheduler.Movie.name + "</h3>";
-
-                //    litNewstMovies.Text += "<p>";
-
-                //    litNewstMovies.Text += "<a href=\"Resever.aspx?id=" + scheduler.Movie.MovieID + "\" class=\"btn btn-primary\">" + scheduler.Movie.Price + "&nbsp;" + "DKK" + "</a>" + "&nbsp;";
-
-                //    litNewstMovies.Text += "<a href=\"Resever.aspx?id=" + scheduler.MovieID + "\" class=\"btn btn-default\">" + "&nbsp;" + "Resever" + "</a>";
-
-                //    litNewstMovies.Text += "</p>";
+                foreach (Movie movie in movieList)
+                {
 
 
-                //    litNewstMovies.Text += "</div>";
+                    litNewstMovies.Text += "<div class=\"col-md-3 col-sm-6\">";
+                    litNewstMovies.Text += "<div class=\"thumbnail\">";
 
-                //    litNewstMovies.Text += "</div>";
-                //    litNewstMovies.Text += "</div>";
+                   // litNewstMovies.Text += "<a href=\"Resever/" + movie.MovieID + movie.name + "\"><img src='../../Content/Images/" + movie.Image + " ' alt='' title='' border=\"0\" /></a>";
+                    litNewstMovies.Text += "<a href=\"Resever/" + movie.name + "\"><img src='../../Content/Images/" + movie.Image + " ' alt='' title='' border=\"0\" /></a>";
+                    litNewstMovies.Text += "<div class=\"caption\">";
+                    litNewstMovies.Text += "<h3>" + movie.name + "</h3>";
+
+                    litNewstMovies.Text += "<p>";
+
+                    litNewstMovies.Text += "<a href=\"Resever.aspx?id=" + movie.MovieID + "\" class=\"btn btn-primary\">" + movie.Price + "&nbsp;" + "DKK" + "</a>" + "&nbsp;";
+
+                    litNewstMovies.Text += "<a href=\"Resever.aspx?id=" + movie.MovieID + "\" class=\"btn btn-default\">" + "&nbsp;" + "Resever" + "</a>";
+
+                    litNewstMovies.Text += "</p>";
+
+
+                    litNewstMovies.Text += "</div>";
+
+                    litNewstMovies.Text += "</div>";
+                    litNewstMovies.Text += "</div>";
 
 
 
 
-                //}
+                }
 
                 litNewstMovies.Text += "</div>";
 
---
+++ ServerProject/ClientWebApp/Login.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ClientWebApp.Login" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <link href="Content/Bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <style type="text/css">
+        body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #eee;
+        }
+
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
+
+            .form-signin .checkbox {
+                font-weight: normal;
+            }
+
+            .form-signin .form-control {
+                position: relative;
+                height: auto;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+                .form-signin .form-control:focus {
+                    z-index: 2;
+                }
+
+            .form-signin input[type="email"] {
+                margin-bottom: -1px;
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+            }
+
+            .form-signin input[type="password"] {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+            }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="container">
+
+                <div class="form-signin">
+
+                    <h2 class="form-signin-heading">Log ind</h2>
+
+
+                    <input name="txtMail" type="text" id="txtMail" class="form-control" placeholder="E-mail adresse" />
+
+                    <p>
+                    </p>
+
+                    <input name="txtPassword" type="text" id="txtPassword" class="form-control" placeholder="Adgangskode" />
+
+                    <p>
+                    </p>
+
+                    <asp:Button ID="btnLogin" runat="server" Text ="Log ind" CssClass="btn btn-lg btn-primary btn-block" OnClick="btnLogin_Click" />
+                    
+
+                    <br />
+                    <a href="#">Glemt adgangskode?</a>
+                    <br />
+
+
+                </div>
+
+
+
+            </div>
+            <!-- /container -->
+
+        </div>
+    </form>
+</body>
+</html>
